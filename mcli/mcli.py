134068b134242f38e522b6a6f477c292a1c461c5
Danlei Qiang <dqa8@sfu.ca>
2022-03-15 17:49:09 -0700

    add mcli support user interaction

--- mcli/mcli.py
+++ mcli/mcli.py
@@ -1,5 +1,6 @@
 """
 Simple command-line interface to music service
+
 """
 
 # Standard library modules
@@ -29,11 +30,15 @@ def parse_args():
         type=int,
         help="Port number of music server"
         )
+    argp.add_argument(
+        'service',
+        help="Microservice name"
+        )
     return argp.parse_args()
 
 
-def get_url(name, port):
-    return "http://{}:{}/api/v1/music/".format(name, port)
+def get_url(name, port, service):
+    return "http://{}:{}/api/v1/{}/".format(name, port,service)
 
 
 def parse_quoted_strings(arg):
@@ -43,7 +48,7 @@ def parse_quoted_strings(arg):
     arguments, such as entries with mismatched quotes.  It's good
     enough for simple use, parsing "-quoted names with apostrophes.
     """
-    mre = re.compile(r'''(\w+)|'([^']*)'|"([^"]*)"''')
+    mre = re.compile(r'''([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})|(\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*)|(\w+)|'([^']*)'|"([^"]*)"''')
     args = mre.findall(arg)
     return [''.join(a) for a in args]
 
@@ -52,6 +57,7 @@ class Mcli(cmd.Cmd):
     def __init__(self, args):
         self.name = args.name
         self.port = args.port
+        self.service = args.service
         cmd.Cmd.__init__(self)
         self.prompt = 'mql: '
         self.intro = """
@@ -83,23 +89,47 @@ Enter 'help' for command list.
         all songs and will instead return an empty list if
         no parameter is provided.
         """
-        url = get_url(self.name, self.port)
-        r = requests.get(
-            url+arg.strip(),
-            headers={'Authorization': DEFAULT_AUTH}
+        url = get_url(self.name, self.port, self.service)
+
+        # Connect music service
+        if self.service == "music":
+            r = requests.get(
+                url+arg.strip(),
+                headers={'Authorization': DEFAULT_AUTH}
+                )
+            if r.status_code != 200:
+                print("Non-successful status code:", r.status_code)
+            items = r.json()
+            if 'Count' not in items:
+                print("0 items returned")
+                return
+            print("{} items returned".format(items['Count']))
+            for i in items['Items']:
+                print("{}  {:20.20s} {}".format(
+                    i['music_id'],
+                    i['Artist'],
+                    i['SongTitle']))
+
+        # Connect user service
+        elif self.service == "user":
+            r = requests.get(
+                url + arg.strip(),
+                headers={'Authorization': DEFAULT_AUTH}
             )
-        if r.status_code != 200:
-            print("Non-successful status code:", r.status_code)
-        items = r.json()
-        if 'Count' not in items:
-            print("0 items returned")
-            return
-        print("{} items returned".format(items['Count']))
-        for i in items['Items']:
-            print("{}  {:20.20s} {}".format(
-                i['music_id'],
-                i['Artist'],
-                i['SongTitle']))
+            if r.status_code != 200:
+                print("Non-successful status code: {}, {}"
+                      .format(r.status_code, r.json()["error"]))
+            items = r.json()
+            if 'Count' not in items:
+                print("0 items returned")
+                return
+            print("{} items returned".format(items['Count']))
+            for i in items['Items']:
+                print("{}  {} {} {}".format(
+                    i['user_id'],
+                    i['fname'],
+                    i['lname'],
+                    i["email"]))
 
     def do_create(self, arg):
         """
@@ -120,18 +150,40 @@ Enter 'help' for command list.
         create Chumbawamba Tubthumping
             No quotes needed for single-word artist or title name.
         """
-        url = get_url(self.name, self.port)
+        url = get_url(self.name, self.port, self.service)
         args = parse_quoted_strings(arg)
-        payload = {
-            'Artist': args[0],
-            'SongTitle': args[1]
-        }
-        r = requests.post(
-            url,
-            json=payload,
-            headers={'Authorization': DEFAULT_AUTH}
-        )
-        print(r.json())
+        if self.service == "music":
+            if len(args) != 2:
+                print("Not enough args provided {}".format(len(args)))
+                return
+
+            payload = {
+                'Artist': args[0],
+                'SongTitle': args[1]
+            }
+            r = requests.post(
+                url,
+                json=payload,
+                headers={'Authorization': DEFAULT_AUTH}
+            )
+            print(r.json())
+
+        elif self.service == "user":
+            if len(args) != 3:
+                print("Not enough or too many args provided {}".format(len(args)))
+                return
+
+            payload = {
+                'fname': args[0],
+                'lname': args[1],
+                'email': args[2]
+            }
+            r = requests.post(
+                url,
+                json=payload,
+                headers={'Authorization': DEFAULT_AUTH}
+            )
+            print(r.json())
 
     def do_delete(self, arg):
         """
@@ -147,13 +199,25 @@ Enter 'help' for command list.
         delete 6ecfafd0-8a35-4af6-a9e2-cbd79b3abeea
             Delete "The Last Great American Dynasty".
         """
-        url = get_url(self.name, self.port)
-        r = requests.delete(
-            url+arg.strip(),
-            headers={'Authorization': DEFAULT_AUTH}
+        url = get_url(self.name, self.port, self.service)
+        args = parse_quoted_strings(arg)
+        if self.service == "music":
+            r = requests.delete(
+                url+arg.strip(),
+                headers={'Authorization': DEFAULT_AUTH}
+                )
+            if r.status_code != 200:
+                print("Non-successful status code: {}, {}"
+                      .format(r.status_code, r.json()["error"]))
+
+        elif self.service == "user":
+            r = requests.delete(
+                url + arg.strip(),
+                headers={'Authorization': DEFAULT_AUTH}
             )
-        if r.status_code != 200:
-            print("Non-successful status code:", r.status_code)
+            if r.status_code != 200:
+                print("Non-successful status code: {}, {}"
+                      .format(r.status_code, r.json()["error"]))
 
     def do_quit(self, arg):
         """
@@ -165,7 +229,7 @@ Enter 'help' for command list.
         """
         Run a test stub on the music server.
         """
-        url = get_url(self.name, self.port)
+        url = get_url(self.name, self.port, self.service)
         r = requests.get(
             url+'test',
             headers={'Authorization': DEFAULT_AUTH}
@@ -177,7 +241,7 @@ Enter 'help' for command list.
         """
         Tell the music cerver to shut down.
         """
-        url = get_url(self.name, self.port)
+        url = get_url(self.name, self.port,self.service)
         r = requests.get(
             url+'shutdown',
             headers={'Authorization': DEFAULT_AUTH}
